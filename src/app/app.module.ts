76b65a1795a3f6ddea27dd6245a4363774ee14b8
Dixita Gohel <Dixita.Gohel@accumatch.com>
2022-03-14 17:58:46 +0530

    change ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomComponent } from './custom/custom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomComponent
   ],
   imports: [
     BrowserModule,
